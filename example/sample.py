0ff2bfabc33ceb66f2dc1edec6842991b878acce
Preocts <preocts@gmail.com>
2021-04-04 01:36:15 -0400

    Clarity in sample code

--- example/sample.py
+++ example/sample.py
@@ -16,25 +16,30 @@ def main() -> None:
         print("You need to set your personal access token, read the code Luke.")
         return
 
+    # Create the client instance, providing your authentication information
     client = GitClient(
-        "[YOUR GITHUB NAME]",
-        "[YOUR GITHUB EMAIL]",
-        "[TARGET REPO OWNER (probably your name)]",
-        "[TARGET REPO]",
-        token,
+        name="[YOUR GITHUB NAME]",
+        email="[YOUR GITHUB EMAIL]",
+        owner="[TARGET REPO OWNER (probably your name)]",
+        repo="[TARGET REPO]",
+        oauth=token,
     )
 
+    # Run the full process
     result = client.send_template(
-        "main",
-        "this_is_a_new_branch",
-        "new_file.md",
-        "# Happy pandas",
+        base_branch="main",
+        new_branch="this_is_a_new_branch",
+        file_name="new_file.md",
+        file_contents="# Happy pandas\n\nThis can be any utf-8 text desired",
         pr_title="This is a sample PR",
         pr_content="All handled through automation.",
         labels=["New"],
     )
 
-    print(f"\n\nFinal status: {result}")
+    if result:
+        print("\n\nEverything ran as expected. Confirm in GitHub!")
+    else:
+        print("\n\nSomething went wrong. Check the logs for clues.")
 
 
 if __name__ == "__main__":

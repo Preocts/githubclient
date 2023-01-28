b09048de1a6450d0d29a3ec01b5913180d01f2a4
Preocts <preocts@preocts.com>
2023-01-28 11:32:52 -0500

    chore: remove urllib3 and use http.client instead (tests failing)

--- src/githubclient/httpclient.py
+++ src/githubclient/httpclient.py
@@ -8,67 +8,86 @@ from __future__ import annotations
 import json
 import logging
 import os
+from http import client
 from typing import Any
-
-import urllib3
 
 
 class HTTPClient:
     """Connect to the GitHub API"""
 
-    BASE_URL = "https://api.github.com"
+    host = "https://api.github.com"
 
     logger = logging.getLogger("APIClient")
 
-    def __init__(self, num_pools: int = 10) -> None:
-        """Personal Auth Token needs to be in 'GITHUB_AUTH_TOKEN` env variable"""
-        auth_token = os.getenv("GITHUB_AUTH_TOKEN", "")
-        user_name = os.getenv("GITHUB_USER_NAME", "")
-        headers = {
+    def __init__(self, host: str | None = None) -> None:
+        """
+        Environment variables are required for authentication.
+
+        GITHUB_AUTH_TOKEN: GitHub personal access token
+        GITHUB_USER_NAME: GitHub username
+
+        Args:
+            host: GitHub API host, defaults to https://api.github.com
+        """
+        self._validate_environs()
+        self.headers = {
             "Accept": "application.vnd.github.v3+json",
-            "User-Agent": user_name,
-            "Authorization": f"token {auth_token}",
+            "User-Agent": os.environ["GITHUB_USER_NAME"],
+            "Authorization": f"token {os.environ['GITHUB_AUTH_TOKEN']}",
         }
+        self.apiclient = client.HTTPSConnection(self._clean_host(host), timeout=15)
 
-        if not auth_token:
-            self.logger.warning("Missing GITHUB_AUTH_TOKEN environment variable")
+    def _clean_host(self, host: str | None) -> str:
+        """Remove the leading protocol from the host and trailing slash"""
+        host = host or self.host
+        return self.host.replace("https://", "").replace("/", "")
 
-        if not user_name:
-            self.logger.warning("Missing GITHUB_USER_NAME environment variable")
+    def _validate_environs(self) -> None:
+        """Check for required environment variables, raise error if missing."""
+        missing_token_msg = "Missing GITHUB_AUTH_TOKEN environment variable"
+        missing_username_msg = "Missing GITHUB_USER_NAME environment variable"
 
-        self.apiclient = urllib3.PoolManager(
-            num_pools=num_pools,
-            headers=headers,
-            retries=urllib3.Retry(total=3, backoff_factor=2),
-        )
+        msg = [] if os.getenv("GITHUB_AUTH_TOKEN", "") else [missing_token_msg]
+        msg.extend([] if os.getenv("GITHUB_USER_NAME", "") else [missing_username_msg])
+
+        if msg:
+            raise OSError(", ".join(msg))
 
     def git_get(self, endpoint: str, payload: dict[str, Any] = {}) -> dict[str, Any]:
         """Handles all GET requests to GitHub"""
 
-        result = self.apiclient.request(
+        self.apiclient.request(
             method="GET",
-            url=self.BASE_URL + endpoint,
+            url=endpoint,
             body=json.dumps(payload),
+            headers=self.headers,
         )
+        response = self.apiclient.getresponse()
+        status_code = response.status
+        result = response.read()
 
-        if result.status not in range(200, 299):
-            self.logger.error("%s - %s", endpoint, result.data)
+        if status_code not in range(200, 299):
+            self.logger.error("%s - %s", endpoint, result)
 
-        return self._jsonify(result.data)
+        return self._jsonify(result)
 
     def git_post(self, endpoint: str, payload: dict[str, Any]) -> dict[str, Any]:
         """Handles all POST requests to GitHub, payload is translated to body"""
 
-        result = self.apiclient.request(
+        self.apiclient.request(
             method="POST",
-            url=self.BASE_URL + endpoint,
+            url=endpoint,
             body=json.dumps(payload),
+            headers=self.headers,
         )
+        response = self.apiclient.getresponse()
+        status_code = response.status
+        result = response.read()
 
-        if result.status not in range(200, 299):
-            self.logger.error("%s - %s", endpoint, result.data)
+        if status_code not in range(200, 299):
+            self.logger.error("%s - %s", endpoint, result)
 
-        return self._jsonify(result.data)
+        return self._jsonify(result)
 
     @staticmethod
     def _jsonify(data: bytes) -> dict[str, Any]:
